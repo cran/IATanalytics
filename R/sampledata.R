3513f877b6e2eef51cf39b653f051f98115c7ca4
Daniel Storage <research@danielstorage.com>
2018-05-22 11:01:43 +0000

    version 0.1.0

---
+++ R/sampledata.R
@@ -0,0 +1,11 @@
+#' Sample Dataset from a Typical IAT
+#'
+#' This dataset contains sample data from a typically-structured IAT.
+#' This IAT has 220 trials, and this particular dataset does not meet criteria for exclusion.
+#'
+#' @name sampledata
+#' @docType data
+#' @usage data(sampledata)
+#' @keywords datasets
+#' @examples data(sampledata)
+"sampledata"
